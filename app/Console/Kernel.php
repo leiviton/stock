35a7daa31e8370efa2cdc66d22c89fdb8bef1b08
Leiviton <leiviton.silva@drsgroup.com.br>
2019-06-06 18:28:46 -0300

    teste cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('protocol:cron')->twiceDaily(7, 18);
+        $schedule->command('protocol:cron')->twiceDaily(7, 19);
     }
 
     /**
