535d36d3347ca8e81a0bb3cabb8de9c5ad03c0e8
Leiviton <leiviton.silva@drsgroup.com.br>
2019-06-24 08:58:52 -0300

    ajustes menu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,12 +8,12 @@
     <meta name="author" content="Creative Tim">
     <title>Portal Estoque</title>
     <!-- Favicon -->
-    <link href="./assets/img/brand/logo.png" rel="icon" type="image/png">
+    <link href="assets/img/brand/logo.png" rel="icon" type="image/png">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <!-- Icons -->
-    <link href="./assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
-    <link href="./assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+    <link href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
     <link rel="stylesheet" href="styles.34e0c95ab52eeb9377de.css">
 </head>
