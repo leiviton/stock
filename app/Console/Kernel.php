d571fdfbcc8ac744042721e89a1ac35a6779bdeb
Leiviton <leiviton.silva@drsgroup.com.br>
2019-06-27 17:01:54 -0300

    get chave logix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('stocks:cron')->timezone('America/Sao_Paulo')
-            ->dailyAt('15:28');
+            ->dailyAt('17:03');
         $schedule->command('roads:cron')->timezone('America/Sao_Paulo')
             ->dailyAt('04:30');
         $schedule->command('outs:cron')->timezone('America/Sao_Paulo')
