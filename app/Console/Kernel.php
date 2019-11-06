982d553c584b08e7d7b273409e311e943ccc29be
Leiviton <leiviton.silva@drsgroup.com.br>
2019-11-06 12:10:03 -0300

    formato data validade entradas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,14 +31,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('stocks:cron')->timezone('America/Sao_Paulo')
+        /*$schedule->command('stocks:cron')->timezone('America/Sao_Paulo')
             ->dailyAt('04:00');
         $schedule->command('roads:cron')->timezone('America/Sao_Paulo')
             ->dailyAt('03:00');
         $schedule->command('outs:cron')->timezone('America/Sao_Paulo')
             ->dailyAt('02:00');
         $schedule->command('protocols:cron')->timezone('America/Sao_Paulo')
-            ->dailyAt('07:00');
+            ->dailyAt('07:00');*/
     }
 
     /**
