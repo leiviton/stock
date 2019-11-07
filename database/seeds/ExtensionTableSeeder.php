ada1bbe4a6cfca6fa133f04cddfd37332e1418f7
Leiviton <leiviton.silva@drsgroup.com.br>
2019-11-07 15:11:06 -0300

    seed contacts

---
+++ database/seeds/ExtensionTableSeeder.php
@@ -0,0 +1,106 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class ExtensionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('extensions')->insert( ['department'=>'DIRETORIA','extension'=>'9001','name'=> 'David','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'DIRETORIA','extension'=>'9006','name'=> 'Deison Sampaio','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'DIRETORIA','extension'=>'9010','name'=> 'Amanda Didone','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'ARQUIVO','extension'=>'9047','name'=> 'Weberta','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'ARQUIVO','extension'=>'9090','name'=> 'Flavia','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'ARQUIVO','extension'=>'9182','name'=> 'Roberta','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMERCIAL','extension'=>'9069','name'=> 'Sergio Miranda','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMERCIAL','extension'=>'9080','name'=> 'Bianca','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMERCIAL','extension'=>'9144','name'=> 'Sergio Nascimento','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMERCIAL','extension'=>'9187','name'=> 'Raissa','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMERCIAL','extension'=>'9188','name'=> 'Tatiane','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMPRAS','extension'=>'9014','name'=> 'Guilherme','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMPRAS','extension'=>'9015','name'=> 'Uerik','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMPRAS','extension'=>'9098','name'=> 'Carolline','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COMPRAS','extension'=>'9117','name'=> 'Paula','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9003','name'=> 'Letícia','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9026','name'=> 'COURIER','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9027','name'=> 'Victor','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9035','name'=> 'Pamella','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9039','name'=> 'Thais','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9048','name'=> 'Manuela','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9061','name'=> 'DOCA G4','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9081','name'=> 'Luana','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9082','name'=> 'Henrique','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9083','name'=> 'Jaqueline','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9104','name'=> 'Itamar','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9107','name'=> 'Vinicius','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9109','name'=> 'Bruno','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9110','name'=> 'Julia','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9111','name'=> 'Joyce','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9115','name'=> 'Paloma','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9116','name'=> 'Ana S.','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9118','name'=> 'Paulo','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9136','name'=> 'Rafael','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9137','name'=> 'Renan','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9138','name'=> 'Gabriel','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9139','name'=> 'Rennan','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9142','name'=> 'Samuel','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9143','name'=> 'Eduardo','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9151','name'=> 'Felipe','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9155','name'=> 'Luan','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9156','name'=> 'Anderson','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9158','name'=> 'Gabriela','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9161','name'=> 'Luan','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9162','name'=> 'Amalia','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9164','name'=> 'Tania','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9165','name'=> 'Camilla','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9168','name'=> 'Silvana','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9169','name'=> 'Weber','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9170','name'=> 'Monica','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9184','name'=> 'Courier - GRU','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9185','name'=> 'Wesllyn','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'COURIER','extension'=>'9195','name'=> 'Emerson','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FARMACIA','extension'=>'9042','name'=> 'Fernando','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FARMACIA','extension'=>'9127','name'=> 'Ayrton','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'DESTRUIÇÃO','extension'=>'9173','name'=> 'Larissa','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'ENVIO','extension'=>'9017','name'=> 'Envio','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'ENVIO','extension'=>'9062','name'=> 'Bianca M.','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'ENVIO','extension'=>'9167','name'=> 'PASP','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'ENVIO','extension'=>'9189','name'=> 'Mariana Amaral','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'ENVIO','extension'=>'9190','name'=> 'Gabriela Andrade','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9020','name'=> 'Marcio','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9023','name'=> 'Ana','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9046','name'=> 'Izabel','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9072','name'=> 'Jesse','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9075','name'=> 'Luzia','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9100','name'=> 'Lucas','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9102','name'=> 'Akemy','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9124','name'=> 'Núbia','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9148','name'=> 'Régis','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9152','name'=> 'Gabriel','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FINANCEIRO','extension'=>'9181','name'=> 'Humberto','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FISCAL','extension'=>'9021','name'=> 'Douglas','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FISCAL','extension'=>'9022','name'=> 'Wellison','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FISCAL','extension'=>'9093','name'=> 'Kamila','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FISCAL','extension'=>'9112','name'=> 'Gustavo','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FISCAL','extension'=>'9153','name'=> 'Guilherme','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FISCAL','extension'=>'9197','name'=> 'Renata','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FISCAL','extension'=>'9163','name'=> 'Fernando','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'FISCAL','extension'=>'9166','name'=> 'Gabrielly','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'IMPORTS','extension'=>'9002','name'=> 'Sidney','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'IMPORTS','extension'=>'9056','name'=> 'Débora','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'IMPORTS','extension'=>'9059','name'=> 'Geovanna','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'IMPORTS','extension'=>'9085','name'=> 'Rafaela','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'IMPORTS','extension'=>'9101','name'=> 'Gisely','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'IMPORTS','extension'=>'9120','name'=> 'Mariana','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'IMPORTS','extension'=>'9145','name'=> 'Kieza','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'KITS','extension'=>'9019','name'=> 'KITS','image'=> 'default.png']);
+        DB::table('extensions')->insert( ['department'=>'KITS','extension'=>'9040','name'=> 'Marilei Carvalho','image'=> 'default.png']);
+
+    }
+}
