59a2acbbf6af2a421110a51888e19e6c74a2b0d0
Leiviton <leiviton.silva@drsgroup.com.br>
2019-12-06 15:32:38 -0300

    docker portal

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,8 +4,6 @@ php artisan cache:clear
 chmod -R 775 storage
 chmod -R 775 bootstrap
 chmod -R 777 vendor
-php artisan migrate
 php artisan key:generate --force
 #php artisan passport:install
-php artisan migrate:fresh --seed
 php-fpm
