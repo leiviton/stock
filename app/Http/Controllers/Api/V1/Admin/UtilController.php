ab3e9427b1864a5727c51ec0b4051e32750e6507
Leiviton <leiviton.silva@drsgroup.com.br>
2019-12-11 16:05:29 -0300

    report

--- app/Http/Controllers/Api/V1/Admin/UtilController.php
+++ app/Http/Controllers/Api/V1/Admin/UtilController.php
@@ -124,8 +124,8 @@ class UtilController extends Controller
             /*$query[$i]['DATA_EMISSAO'] = strrev($query[$i]['DATA_EMISSAO']);
             $query[$i]['DATA_LANCTO'] = strrev($query[$i]['DATA_LANCTO']);*/
 
-            $query[$i]['DATA_EMISSAO'] = substr($query[$i]['DATA_EMISSAO'],7,2).'/'.substr($query[$i]['DATA_EMISSAO'],4,2).'/'.substr($query[$i]['DATA_EMISSAO'],0,4);
-            $query[$i]['DATA_LANCTO'] = substr($query[$i]['DATA_LANCTO'],7,2).'/'.substr($query[$i]['DATA_LANCTO'],4,2).'/'.substr($query[$i]['DATA_LANCTO'],0,4);
+            $query[$i]['DATA_EMISSAO'] = substr($query[$i]['DATA_EMISSAO'],6,2).'/'.substr($query[$i]['DATA_EMISSAO'],4,2).'/'.substr($query[$i]['DATA_EMISSAO'],0,4);
+            $query[$i]['DATA_LANCTO'] = substr($query[$i]['DATA_LANCTO'],6,2).'/'.substr($query[$i]['DATA_LANCTO'],4,2).'/'.substr($query[$i]['DATA_LANCTO'],0,4);
         }
 
         return $query;
