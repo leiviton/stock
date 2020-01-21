ae6c614d4650f09a5bd6ac10e90d2c5bde63d05d
Leiviton <leiviton.silva@drsgroup.com.br>
2020-01-21 19:58:16 -0300

    intranet

---
+++ resources/views/information.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+    <base href='/intranet'>
+    <meta charset="utf-8"/>
+    <link rel="apple-touch-icon" sizes="76x76" href="./intranet/assets/img/apple-icon.png"/>
+    <link rel="icon" type="image/png" href="./intranet/assets/img/favicon.png"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+    <title>Now UI Dashboard Angular by Creative Tim</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet"/>
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+
+</head>
+<body>
+<app-root>
+</app-root>
+<script type="text/javascript" src="./intranet/runtime.js"></script>
+<script type="text/javascript" src="./intranet/polyfills.js"></script>
+<script type="text/javascript" src="intranet/styles.js"></script>
+<script type="text/javascript" src="intranet/scripts.js"></script>
+<script type="text/javascript" src="intranet/vendor.js"></script>
+<script type="text/javascript" src="intranet/main.js"></script>
+</body>
+
+
+</html>
